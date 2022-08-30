afa8d2c912db713223f58ea510c25157ee7f0a37
maxi326519 <97068314+maxi326519@users.noreply.github.com>
2022-08-30 12:36:15 -0300

    Homework 01 Terminado

--- FT-M1-master/01-IntroToCS/homework/homework.js
+++ FT-M1-master/01-IntroToCS/homework/homework.js
@@ -2,14 +2,26 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
+  var suma = 0;
 
+  for(var i = 0; i < num.length; i++){
+    suma += (Math.pow(2,(num.length - i -1)))*num[i];
+  }
+
+  return suma;
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
+  var binaryStr = [];
 
+  while(num != 0){
+    binaryStr.unshift(num%2);
+    num = Math.floor(num/2);
+  };
+
+  return binaryStr.join('');
 }
-
 
 module.exports = {
   BinarioADecimal,
