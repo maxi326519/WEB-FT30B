75f08ad43f2d3c4f236d9387c71a992abf292e22
maxi326519 <97068314+maxi326519@users.noreply.github.com>
2022-09-06 17:41:36 -0300

    Homework 03 - RESUELTO

--- FT-M1-master/03-JavaScriptAvanzado-II/homework/homework.js
+++ FT-M1-master/03-JavaScriptAvanzado-II/homework/homework.js
@@ -19,16 +19,27 @@ function counter() {
   otroContador()      // 2
   otroContador()      // 3
    */
+  var contador = 0;
+
+  return function(){
+    contador++;
+    return contador;
+  }
 }
 
 function cacheFunction(cb) {
   /*
   Ejercicio 2
 
-  Tu tarea aquí es lograr, mediante un closure, que cacheFunction actúe como una memoria caché para el callback que recibe por parámetro (cb); es decir, que "recuerde" el resultado de cada operación que hace, de manera que, al realizar una operación por segunda vez, se pueda obtener el resultado de esa "memoria" sin tener que efectuar otra vez cálculos que ya se hicieron anteriormente.
+  Tu tarea aquí es lograr, mediante un closure, que cacheFunction actúe como una memoria caché para el callback que recibe
+  por parámetro (cb); es decir, que "recuerde" el resultado de cada operación que hace, de manera que,
+  al realizar una operación por segunda vez, se pueda obtener el resultado de esa "memoria" sin tener que efectuar otra vez cálculos
+  que ya se hicieron anteriormente.
 
-  cacheFunction debe retornar una función. Esta función debe aceptar un argumento (arg) e invocar a cb con ese argumento; hecho eso, debe guardar el argumento junto con el resultado de la invocación (tip: usá un objeto donde cada propiedad sea el argumento, y su valor el resultado de la correspondiente invocación a cb) de manera que, la próxima vez que reciba el mismo argumento, no sea necesario volver a invocar a cb, porque el resultado estará guardado en la "memoria caché".
-
+  cacheFunction debe retornar una función. Esta función debe aceptar un argumento (arg) e invocar a cb con ese argumento;
+  hecho eso, debe guardar el argumento junto con el resultado de la invocación (tip: usá un objeto donde cada propiedad sea el argumento,
+  y su valor el resultado de la correspondiente invocación a cb) de manera que, la próxima vez que reciba el mismo argumento,
+  no sea necesario volver a invocar a cb, porque el resultado estará guardado en la "memoria caché".
 
   Ejemplo:
   function square(n){
@@ -39,12 +50,22 @@ function cacheFunction(cb) {
 
   squareCache(5)    // invocará a square(5), almacenará el resultado y lo retornará
   squareCache(5)    // no volverá a invocar a square, simplemente buscará en la caché cuál es el resultado de square(5) y lo retornará (tip: si usaste un objeto, podés usar hasOwnProperty) 
-
   */
+
+  var cache = {};
+
+  return function(arg){
+
+    if(cache.hasOwnProperty(arg)){
+      return cache[arg];
+    }else{
+      cache[arg] = cb(arg);
+      return cache[arg];
+    }
+  }
 }
 
 // Bind
-
 var instructor = {
   nombre: "Franco",
   edad: 25,
@@ -64,16 +85,22 @@ function getNombre() {
 
   IMPORTANTE: no modificar el código de arriba (variables instructor y alumno, y función getNombre)
 
-  Usando el método bind() guardar, en las dos variables declaradas a continuación, dos funciones que actúen como getNombre pero retornen el nombre del instructor y del alumno, respectivamente.
+  Usando el método bind() guardar, en las dos variables declaradas a continuación,
+  dos funciones que actúen como getNombre pero retornen el nombre del instructor y del alumno, respectivamente.
 */
 
 let getNombreInstructor;
 let getNombreAlumno;
 
+getNombreInstructor = getNombre.bind(instructor);
+getNombreAlumno = getNombre.bind(alumno);
+
 /*
   Ejercicio 4
   
-  Sin modificar la función crearCadena, usar bind para guardar, en las tres variables declaradas a continuación, tres funciones que retornen una cadena (string) y el delimitador especificado (asteriscos, guiones, y guiones bajos, respectivamente). Las funciones obtenidas deberían recibir solamente un argumento - la cadena de texto - ya que los otros argumentos habrán sido "bindeados". 
+  Sin modificar la función crearCadena, usar bind para guardar, en las tres variables declaradas a continuación,
+  tres funciones que retornen una cadena (string) y el delimitador especificado (asteriscos, guiones, y guiones bajos, respectivamente).
+  Las funciones obtenidas deberían recibir solamente un argumento - la cadena de texto - ya que los otros argumentos habrán sido "bindeados". 
 */
 
 function crearCadena(delimitadorIzquierda, delimitadorDerecha, cadena) {
@@ -83,6 +110,10 @@ function crearCadena(delimitadorIzquierda, delimitadorDerecha, cadena) {
 let textoAsteriscos;
 let textoGuiones;
 let textoUnderscore;
+
+textoAsteriscos = crearCadena.bind(this, '*', '*');
+textoGuiones    = crearCadena.bind(this, '-', '-');
+textoUnderscore = crearCadena.bind(this, '_', '_');
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
