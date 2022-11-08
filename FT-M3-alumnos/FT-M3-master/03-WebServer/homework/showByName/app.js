9eff6fd0b2e1e2089b1e2c11807027513f92486b
maxi326519 <97068314+maxi326519@users.noreply.github.com>
2022-11-08 13:37:01 -0300

    homework - show images

--- FT-M3-alumnos/FT-M3-master/03-WebServer/homework/showByName/app.js
+++ FT-M3-alumnos/FT-M3-master/03-WebServer/homework/showByName/app.js
@@ -2,3 +2,27 @@ var fs  = require("fs")
 var http  = require("http")
 
 // Escribí acá tu servidor
+const PORT = 2000;
+
+var img = fs.readdirSync('./images');
+
+http.createServer((req, res)=>{
+    console.log(req.url);
+
+    if(req.url === '/'){
+        response(res, 200, `text/plane`, JSON.stringify(img));
+    }else{
+        let search = img.find( name => name.split('.')[0] === req.url.slice(1).replace('%20', '_'));
+        if(search){
+            let file = fs.readFileSync(`./images/${search}`)
+            response(res, 200, `image/${search.split('.')[1]}`, file);
+        }else{
+            response(res, 404, `text/plane`, 'error 404: page not found');
+        }
+    }
+}).listen(PORT, '127.0.0.1')
+
+function response(res, status, type, data){
+    res.writeHead(status, { 'Content-Type': type})
+    res.end(data);
+}
