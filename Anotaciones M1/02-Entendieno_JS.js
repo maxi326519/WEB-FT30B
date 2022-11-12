7783513e4bff9c175760f6728023cb6d729ea371
maxi326519 <97068314+maxi326519@users.noreply.github.com>
2022-11-12 02:13:42 -0300

    Delete

--- Anotaciones M1/02-Entendieno_JS.js
+++
@@ -1,8 +0,0 @@
-// Closures
-// Es cuando una funcion utiliza elementos del lexico environment
-// del bloque donde fue declarado
-
-
-// Bind, Call & Apply
-// BIND:
-// Cambia a donde apunta el this
